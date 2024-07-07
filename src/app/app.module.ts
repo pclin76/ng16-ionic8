4510d6102f6b6129b6e0a59bd558d8eed330d110
高科技黑手 <45622996+pclin76@users.noreply.github.com>
2024-07-07 16:29:56 +0800

    初次提交

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {IonicModule} from '@ionic/angular';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot({})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule {
+}
